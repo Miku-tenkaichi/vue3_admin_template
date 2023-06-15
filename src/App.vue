9e436272ee0f07c0f099e02c9a6e80c14aed5941
Miku-tenkaichi <1209806343@qq.com>
2023-06-15 11:32:48 +0800

    feat: 登录页搭建

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,6 @@ const a = 1
 console.log(a)
 </script>
 <template>
-    <h1>
-        <el-button type="primary">Primary</el-button>
-    </h1>
-    <svg-icon name="phone" color="pink" width="100px" height="100px"></svg-icon>
     <router-view></router-view>
 </template>
 <style scoped lang="scss"></style>
